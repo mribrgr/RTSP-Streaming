fce7a12b385708c20f161f28d98a29e53230631a
Mauritius Berger <mauritius.berger@develappers.de>
2023-01-11 21:41:05 +0100

    update init script to be able to stop everything with ctrl c

--- start-compile.sh
+++ start-compile.sh
@@ -4,7 +4,7 @@
 # für Eclipse sind die Verzeichnisse entsprechend anzupassen
 
 port=8554
-#video=videos/htw.mjpeg
+# video=videos/htw.mjpeg
 video=htw.mjpeg
 src=src
 bin=out/production/RTSP-Streaming
@@ -18,5 +18,11 @@ javac -cp $src ${src}/Client.java  -d $bin
 # Start
 echo "start classes..."
 java -cp $bin  Server $port &
-sleep 1s
+sleep 1
 java -cp $bin  Client localhost $port $video &
+
+# wait for ctrl+c is pressed, then kill all java processes and exit
+trap "killall java; exit" SIGINT SIGTERM
+
+# keep it open
+while true; do sleep 1; done
